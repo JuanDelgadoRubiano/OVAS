6ed4253961b6b3db8d7d75574a93774c84b0c8cc
JuanDelgadoRubiano <juan.rubiano@correounivalle.edu.co>
2022-02-03 13:52:35 -0500

    test

--- src/database/database.config.js
+++ src/database/database.config.js
@@ -6,7 +6,8 @@ const pool = new Pool({
     host: process.env.hostbd || 'ec2-34-227-120-94.compute-1.amazonaws.com',
     password: process.env.passwordbd ||'633f5833e8e223d0ff2d62f69dd721efc355dc502391abcc68757414b8633234',
     database: process.env.databasebd ||'d3vmfeep32u6ko',
-    port: process.env.port ||5432
+    port: process.env.port ||5432,
+    ssl: { rejectUnauthorized: false }
 });
 module.exports = {
     pool
