79b1ec451cedd4d87a93057685e39e2a35b57ad4
JuanDelgadoRubiano <juan.rubiano@correounivalle.edu.co>
2022-02-03 13:41:01 -0500

    credentials

--- src/database/database.config.js
+++ src/database/database.config.js
@@ -2,10 +2,10 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.userbd || 'postgres',
-    host: process.env.hostbd || 'localhost',
-    password: process.env.passwordbd ||'11111',
-    database: process.env.databasebd ||'ovaDataBase',
+    user: process.env.userbd || 'mknvgzphefxnep',
+    host: process.env.hostbd || 'ec2-34-227-120-94.compute-1.amazonaws.com',
+    password: process.env.passwordbd ||'633f5833e8e223d0ff2d62f69dd721efc355dc502391abcc68757414b8633234',
+    database: process.env.databasebd ||'d3vmfeep32u6ko',
     port: process.env.port ||5432
 });
 module.exports = {
